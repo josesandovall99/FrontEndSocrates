f8149ae01d98a2ba0de7a633c4c483cae036ee4b
Edgar Parada <edgaryamillparadaduarte@gmail.com>
2025-03-15 16:53:44 -0500

    cree el modelo de servicio

---
+++ src/app/models/servicio.model.ts
@@ -0,0 +1,14 @@
+import { TipoPlan } from './TipoPlan';
+import { Tecnico } from './tecnico.model';
+// import { Cliente } from './cliente.model'; // Comentado porque aún no existe
+
+export interface Servicio {
+  id: number;
+  fechaServicio: Date;
+  descripcion: string;
+  horaServicio: string; // Usaremos string para simplificar
+  estado: string;
+  tipoPlan: TipoPlan;
+  tecnico: Tecnico;
+  // cliente: Cliente; // Comentado porque aún no existe
+}
