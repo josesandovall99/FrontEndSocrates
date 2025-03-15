1dbe6412853bd723c64eb5f7373543b893743bd1
Edgar Parada <edgaryamillparadaduarte@gmail.com>
2025-03-15 16:25:13 -0500

    Cree el service de TipoPlan

---
+++ src/app/services/TipoPlanService.ts
@@ -0,0 +1,29 @@
+import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable } from 'rxjs';
+import { TipoPlan } from '../models/TipoPlan';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class TipoPlanService {
+  private apiUrl = 'api/v1/tipos_planes'; // URL de la API
+
+  constructor(private http: HttpClient) {}
+
+  list(): Observable<TipoPlan[]> {
+    return this.http.get<TipoPlan[]>(this.apiUrl);
+  }
+
+  create(tipoPlan: TipoPlan): Observable<TipoPlan> {
+    return this.http.post<TipoPlan>(this.apiUrl, tipoPlan);
+  }
+
+  update(id: number, tipoPlan: TipoPlan): Observable<TipoPlan> {
+    return this.http.put<TipoPlan>(`${this.apiUrl}/${id}`, tipoPlan);
+  }
+
+  delete(id: number): Observable<void> {
+    return this.http.delete<void>(`${this.apiUrl}/${id}`);
+  }
+}
